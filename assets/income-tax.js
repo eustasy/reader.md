2e002f9909eb89ed53c976a6c0844287918e656d
Lewis Goddard <lewisgoddard@eustasy.org>
2017-09-21 04:13:54 +0100

    Create income-tax.js

---
+++ assets/income-tax.js
@@ -0,0 +1,79 @@
+function TaxBracket(income, bracket, tax) {
+	if ( income > bracket ) {
+		console.log(tax + '% tax on ' + ( income - bracket ) + ' for ' + bracket)
+		return ( ( income - bracket ) * tax )
+	} else 
+		return 0
+	}
+}
+var income = 0
+var tax = 0
+function IncomeTaxCalculator() {
+
+	console.log(document.getElementById('income').value)
+	field = document.getElementById('income').value
+
+	income = field
+	tax = 0
+	tax_bands = []
+	brackets = []
+	brackets.push(150000)
+	brackets.push(45000)
+	brackets.push(11500)
+	tax_bands.push(45)
+	tax_bands.push(40)
+	tax_bands.push(20)
+	var bracket_count = brackets.length
+	for (var i = 0; i < bracket_count; i++) {
+		if ( income > brackets[i] ) {
+			tax = tax + TaxBracket(income, brackets[i], tax_bands[i])
+			income = brackets[i]
+		}
+	}
+	if ( field > 100000 ) {
+		overhold = field - 100000
+		overhold = overhold / 2
+		// Tax the pervious personal allowance at the normal rate.
+		if ( overhold > 11500 ) {
+			tax = tax + ( 11500 * 20 )
+		} else {
+			tax = tax + ( overhold * 20 )
+		}
+	}
+	tax = tax / 100
+	document.getElementById('js-target-tax').innerHTML = '£ ' + tax
+
+	// Proposed
+	income = field
+	tax = 0
+	tax_bands = []
+	brackets = []
+	brackets.push(123000)
+	brackets.push(80000)
+	brackets.push(45000)
+	brackets.push(11500)
+	tax_bands.push(50)
+	tax_bands.push(45)
+	tax_bands.push(40)
+	tax_bands.push(20)
+	var bracket_count = brackets.length
+	for (var i = 0; i < bracket_count; i++) {
+		if ( income > brackets[i] ) {
+			tax = tax + TaxBracket(income, brackets[i], tax_bands[i])
+			income = brackets[i]
+		}
+	}
+	if ( field > 100000 ) {
+		overhold = field - 100000
+		overhold = overhold / 2
+		// Tax the pervious personal allowance at the normal rate.
+		if ( overhold > 11500 ) {
+			tax = tax + ( 11500 * 20 )
+		} else {
+			tax = tax + ( overhold * 20 )
+		}
+	}
+	tax = tax / 100
+	document.getElementById('js-target-new').innerHTML = '£ ' + tax
+
+}
