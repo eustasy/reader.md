62e97cd8aa2c0b9461530a5bbe0eab0d0d21d17b
Lewis Goddard <lewisgoddard@eustasy.org>
2018-05-21 21:42:05 +0100

    Update footer.php

--- _templates/footer.php
+++ _templates/footer.php
@@ -1,4 +1,4 @@
 		<footer>Copyright &copy; <?php echo Date('Y'); ?> eustasy <span class="float-right">MIT Licensed</a></footer>
-		<script src="https://cdn.jsdelivr.net/g/rem"></script>
+		<script src="https://cdn.jsdelivr.net/gh/chuckcarpenter/REM-unit-polyfill@1/js/rem.min.js"></script>
 	</body>
 </html>
